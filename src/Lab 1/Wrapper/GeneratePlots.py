1d4f92c8d142dc2c1a12a9c9149f53f325ea935f
Erwin de Haan <EraYaN@users.noreply.github.com>
2016-09-26 11:41:51 +0200

    Updated PDF plots.

--- src/Lab 1/Wrapper/GeneratePlots.py
+++ src/Lab 1/Wrapper/GeneratePlots.py
@@ -8,8 +8,20 @@ import numpy as np
 
 from WrapperShared import Variant
 from terminaltables import AsciiTable
-import matplotlib2tikz as tikz
-from scipy.interpolate import interp1d
+#import matplotlib2tikz as tikz
+from scipy.optimize import curve_fit
+
+def func_exp(x, a, b):
+    return a*np.exp(x*b)
+
+def func_log(x, a, b):
+    return a*np.log(x*b)
+
+def func_poly3(x, a, b, c, d):
+    return a*x**3 + b*x**2 +c*x + d
+
+def func_poly5(x, a, b, c, d, e, f):
+    return a*x**5 + b*x**4 + c*x**3 + d*x**2 + e*x + f
 
 def GeneratePlot(results, job_title, output_dir = '.'):
     # Task specific graph settings
@@ -19,45 +31,53 @@ def GeneratePlot(results, job_title, output_dir = '.'):
         xlabel = "Threads"
         ylabel = "Speedup"
         title = "OpenMP vs Sequential (data_size={0})".format(results[0]['data_size'])
+        order = func_poly3
     elif job_title == "openmp-data-size-sweep":
         xfield = "data_size"
         yfield= "relative_improvement"
         xlabel = "Data Size"
         ylabel = "Speedup"
         title = "OpenMP vs Sequential (threads={0})".format(results[0]['threads'])
+        order = func_poly5
     elif job_title == "sse-data-size-sweep":
         xfield = "data_size"
         yfield= "relative_improvement"
         xlabel = "Matrix/Vector Size"
         ylabel = "Speedup"
         title = "SSE vs Sequential"
+        order = func_poly5
     elif job_title == "sse-data-size-sweep-arbitrary":
         xfield = "data_size"
         yfield= "relative_improvement"
         xlabel = "Matrix/Vector Size"
         ylabel = "Speedup"
         title = "SSE vs Sequential Arbitrary Size"
+        order = func_poly5
     elif job_title == "sse-dp":
         xfield = "data_size"
         yfield= "relative_improvement"
         xlabel = "Matrix/Vector Size"
         ylabel = "Speedup"
         title = "SSE vs Sequential Double Precision"
+        order = func_poly5
     elif job_title == "opencl-data-size-sweep":
         xfield = "data_size"
         yfield= "relative_improvement"
         xlabel = "Matrix/Vector Size"
         ylabel = "Speedup"
         title = "OpenCL vs Sequential (localSize={0})".format(results[0]['threads'])
+        order = func_poly5
     elif job_title == "opencl-localsize-sweep":
         xfield = "threads"
         yfield= "relative_improvement"
-        xlabel = "Matrix/Vector Size"
+        xlabel = "Local Work Group Size"
         ylabel = "Speedup"
         title = "OpenCL vs Sequential (data_size={0})".format(results[0]['data_size'])
+        order = func_log
     else:
         return 'ERROR: job_title unknown'
 
+    print("Processing Data...")
     # Generate graph
     graphX = []
     graphY = []
@@ -66,22 +86,30 @@ def GeneratePlot(results, job_title, output_dir = '.'):
         graphX.append(result[xfield])
         graphY.append(result[yfield])
 
+    #print("Fitting Curve...")
     x_sm = np.array(graphX)
     y_sm = np.array(graphY)
-    graphX_smooth = np.linspace(x_sm.min(), x_sm.max(), len(graphY)*10)
-    f = interp1d(graphX, graphY, kind='quadratic')
-    graphY_smooth = f(graphX_smooth);
+    #graphX_smooth = np.linspace(x_sm.min(), x_sm.max(), len(graphY)*10)
+    #graphY_smooth = spline(graphX, graphY, graphX_smooth, kind = "smoothest")
+    #p = np.polyfit(graphX, graphY, order)
+    #f = np.poly1d(p)
 
+    #popt, pcov = curve_fit(order, graphX, graphY)
+    #graphY_smooth = order(graphX_smooth, *popt);
+
+    print("Generating Plot...")
     figure = plt.figure()
     style.use('ggplot')
-    plt.plot(graphX_smooth,graphY_smooth)
+    plt.scatter(graphX,graphY)
+    #plt.plot(graphX_smooth,graphY_smooth)
     plt.xlim([x_sm.min(),x_sm.max()])
-    plt.ylim([y_sm.min()-(y_sm.max()-y_sm.min())*0.01,y_sm.max()+(y_sm.max()-y_sm.min())*0.01])
+    plt.ylim([y_sm.min()-(y_sm.max()-y_sm.min())*0.05,y_sm.max()+(y_sm.max()-y_sm.min())*0.05])
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
     plt.grid(True)
 
+    print("Saving Plot...")
     # convert graph to tikz
     figure.savefig('{0}/{1}.pdf'.format(output_dir,job_title),format='pdf',transparent=True)
-    tikz.save('{0}/{1}.tikz'.format(output_dir,job_title),figure=figure,figureheight = '\\figureheight',figurewidth = '\\figurewidth',show_info=True,encoding='utf-8',draw_rectangles=True)
+    #tikz.save('{0}/{1}.tikz'.format(output_dir,job_title),figure=figure,figureheight = '\\figureheight',figurewidth = '\\figurewidth',show_info=True,encoding='utf-8',draw_rectangles=True)
