d624ee351ae8eed9c1ace88427bb7ae38fcd32d4
Renzo Plaisant van der Wal <r.v.plaisantvanderwal@gmail.com>
2016-10-06 09:49:05 +0200

    small bug fixes

--- src/Lab 2/benchmark/cuda/cuda-histogram.cu
+++ src/Lab 2/benchmark/cuda/cuda-histogram.cu
@@ -8,7 +8,7 @@ __global__ void histogram1DCudaKernel(unsigned char *grayImage, unsigned int *hi
 	int y = blockIdx.y * blockDim.y + threadIdx.y; // height
 
 	if (x < width && y < height) {
-		atomicAdd(histogram[static_cast<unsigned int>(grayImage[(y * width) + x])], 1);
+		atomicAdd((unsigned int *)histogram[static_cast<unsigned int>(grayImage[(y * width) + x])], (unsigned int)1);
 	}
 }
 
