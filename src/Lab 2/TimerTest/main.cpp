36a7c9a38ab80703d10f42f25cfa3c71fb9d6478
Erwin de Haan <EraYaN@users.noreply.github.com>
2016-10-08 12:59:10 +0200

    Added NSTimer to timer test.

--- src/Lab 2/TimerTest/main.cpp
+++ src/Lab 2/TimerTest/main.cpp
@@ -8,6 +8,15 @@
 #ifdef _WIN32
 #include "Windows.h"
 #endif
+//for that hacky unix approach
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+
+#include "Timer.hpp"
 
 #include <stdint.h>
 
@@ -30,6 +39,7 @@ uint64_t rdtsc() {
 #endif
 
 using namespace std;
+using LOFAR::NSTimer;
 
 template <class Clock>
 void
@@ -43,7 +53,7 @@ display_precision() {
 static int sink = 0;
 
 int main() {
-	double freq = 1;
+	double freq = 0;
 #if (defined __linux__ || defined __blrts__) && \
 	(defined __i386__ || defined __x86_64__ || defined __ia64__ || defined __PPC__) && \
 	(defined __GNUC__ || defined __INTEL_COMPILER || defined __PATHSCALE__ || defined __xlC__)
@@ -52,29 +62,31 @@ int main() {
 
 	while (infile.good()) {
 		infile.getline(buffer, 256);
-
+		cout << "Read from file: " << buffer << endl;
 		if (strncmp("cpu MHz", buffer, 7) == 0 && (colon = strchr(buffer, ':')) != 0) {
 			freq = atof(colon + 2)*1e6;
 			cout << "Reported frequency (UNIX): " << (freq / 1e6) << " MHz" << endl;
 			break;
-		} else {
-			throw "Can't get frequency from proc/cpuinfo.";
-}
-}
+		}		
+	}
+	if (freq==0) {
+		cerr << "Can't get frequency from /proc/cpuinfo." << endl;
+		throw "Can't get frequency from /proc/cpuinfo.";
+	}
+
 #elif defined _WIN32
 	LARGE_INTEGER frequency;
-	if (::QueryPerformanceFrequency(&frequency) == FALSE)
+	if (::QueryPerformanceFrequency(&frequency) == FALSE) {
+		cerr << "Can't get frequency from QueryPerformanceFrequency." << endl;
 		throw "Can't get frequency from QueryPerformanceFrequency.";
+	}
 
 	//frequency is in kilo hertz
 	cout << "Reported frequency (Win32): " << frequency.QuadPart / 1e3 << " MHz" << endl;
 	freq = (double)frequency.QuadPart * 1e3;
 #endif
 
-
-
-	cout << "Chrono min time: " << ((double)chrono::high_resolution_clock::period::num
-		/ chrono::high_resolution_clock::period::den)*1e9 << " ns" << endl;
+	cout << "Chrono min time: " << ((double)chrono::high_resolution_clock::period::num / chrono::high_resolution_clock::period::den)*1e9 << " ns" << endl;
 	cout << "High resolution clock: ";
 	display_precision<std::chrono::high_resolution_clock>();
 	cout << "System clock: ";
@@ -83,6 +95,7 @@ int main() {
 	cout << endl;
 	//TODO test against NSTimer, because of the deep pipeline it seems also inaccurate.
 	typedef chrono::high_resolution_clock PerfClock;
+	
 	int size = 250;
 	int func_iter = 1000;
 	int tests = 1000;
@@ -90,8 +103,11 @@ int main() {
 
 	double avg_chrono = 0;
 	double avg_rdtsc = 0;
+	double avg_nstimer = 0;
 
 	for (int test = 0;test < tests;test++) {
+		NSTimer nsTimer = NSTimer("TestTimer", false, false);
+		nsTimer.start();
 		auto t1 = PerfClock::now();
 		t1r = rdtsc();
 		for (int i = 0;i < func_iter;i++) {
@@ -100,22 +116,31 @@ int main() {
 		}
 		t2r = rdtsc();
 		auto t2 = PerfClock::now();
+		nsTimer.stop();
 
+		
 		chrono::duration<double, nano> measurement = t2 - t1;
 
-		avg_chrono += measurement.count()/tests;
+		double nstimer_ns = nsTimer.getElapsed()*1e9;
+		avg_nstimer += nstimer_ns / tests;
+		avg_chrono += measurement.count() / tests;
 		double ns_rdtsc = (t2r - t1r) / (freq) * 1e9;
-		avg_rdtsc += ns_rdtsc/tests;
+		avg_rdtsc += ns_rdtsc / tests;
 
+		cout << "[" << test + 1 << "] NSTimer: f() took " << nstimer_ns << " ns for " << func_iter << " function iterations." << endl;
 		cout << "[" << test + 1 << "] Chrono: f() took " << measurement.count() << " ns for " << func_iter << " function iterations." << endl;
 		cout << "[" << test + 1 << "] RDTSC: f() took " << ns_rdtsc << " ns for " << func_iter << " function iterations." << endl;
 		cout << "[" << test + 1 << "] and the result was " << sink << " for " << size << " vector size." << endl;
 		cout << endl;
+
 	}
 
 	cout << endl;
+	cout << "[AVG] NSTimer: f() took " << avg_nstimer << " ns." << endl;
 	cout << "[AVG] Chrono: f() took " << avg_chrono << " ns." << endl;
 	cout << "[AVG] RDTSC: f() took " << avg_rdtsc << " ns." << endl;
+#ifdef _WIN32
 	cout << "Press enter to continue." << endl;
 	cin.get();
+#endif
 }
